e8dbfc67ad28743e0ab343de6fd668deccde5eff
tcfischl <bruno.fischlin@stud.hslu.ch>
2018-03-09 17:14:05 +0100

    Daily end commit

--- BetChain/app/src/main/java/bda/hslu/ch/betchain/MainActivity.java
+++ BetChain/app/src/main/java/bda/hslu/ch/betchain/MainActivity.java
@@ -42,6 +42,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Fragment fragment = new LoginFragment();
+
+        changeFragment(fragment);
     }
 
     @Override
