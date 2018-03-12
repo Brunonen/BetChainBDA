13f87e68d96ac2bdf1d9c45655d79e4c94d90a76
tcfischl <bruno.fischlin@stud.hslu.ch>
2018-03-12 14:38:29 +0100

    Created Directory for WebFunctions and created Dummy method

--- BetChain/app/src/main/java/bda/hslu/ch/betchain/WebFunctions/FriendFunctions.java
+++ BetChain/app/src/main/java/bda/hslu/ch/betchain/WebFunctions/FriendFunctions.java
@@ -1,10 +1,26 @@
 package bda.hslu.ch.betchain.WebFunctions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import bda.hslu.ch.betchain.DTO.Friend;
+import bda.hslu.ch.betchain.R;
+
 /**
  * Created by ssj10 on 12/03/2018.
  */
 
 public class FriendFunctions {
 
+    public static List<Friend> getUserFriendList(){
 
+        List<Friend> friendList = new ArrayList<Friend>();
+
+        friendList.add(new Friend("Bruno Fischlin", R.drawable.bruno));
+        friendList.add(new Friend("Damir Hodzic", R.drawable.damir));
+        friendList.add(new Friend("Alex Neher" , R.drawable.alex));
+        friendList.add(new Friend("Suki Kasipillai" , R.drawable.suki));
+        
+        return friendList;
+    }
 }
