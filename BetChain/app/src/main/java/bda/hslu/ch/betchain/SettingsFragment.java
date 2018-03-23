750fb16656c2708c1ac09000a04a58fb11bd6263
tcfischl <bruno.fischlin@stud.hslu.ch>
2018-03-23 14:53:04 +0100

    Change Settings implemented. ShowOnline can now be changed and it influences if you're found in the online search

--- BetChain/app/src/main/java/bda/hslu/ch/betchain/SettingsFragment.java
+++ BetChain/app/src/main/java/bda/hslu/ch/betchain/SettingsFragment.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import bda.hslu.ch.betchain.DTO.User;
+import bda.hslu.ch.betchain.Database.SQLWrapper;
+import bda.hslu.ch.betchain.WebFunctions.SettingsFunctions;
+import bda.hslu.ch.betchain.WebFunctions.UserFunctions;
 
 
 public class SettingsFragment extends Fragment {
@@ -15,8 +23,41 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        final MainActivity activity = (MainActivity) getActivity();
+        Switch showOnline = (Switch) rootView.findViewById(R.id.switchShowOnline);
+        Switch enablePushNotifications = (Switch) rootView.findViewById(R.id.switchEnablePushNotifications);
 
+        try {
+            User userInfos = UserFunctions.getUserInfo(getLoggedInUserInfo()[0]);
+            showOnline.setChecked(userInfos.isShowOnline());
+        } catch (WebRequestException e) {
+            e.printStackTrace();
+        }
+        showOnline.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                int showOnlineValue = (isChecked) ? 1 : 0;
+                try {
+                    SettingsFunctions.changeUserSetting("showOnline", String.valueOf(showOnlineValue));
+                } catch (WebRequestException e) {
+                    Toast.makeText(activity,e.getMessage() , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        enablePushNotifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //Enable / Disable Push notiifactions
+            }
+        });
 
         return rootView;
+    }
+
+    private String[] getLoggedInUserInfo(){
+        String[] returnString;
+        MainActivity activity = (MainActivity) getActivity();
+        SQLWrapper db = new SQLWrapper(activity);
+        returnString = db.getLoggedInUserInfo();
+        return returnString;
     }
 }
---
+++ BetChain/app/src/main/java/bda/hslu/ch/betchain/WebFunctions/SettingsFunctions.java
@@ -0,0 +1,38 @@
+package bda.hslu.ch.betchain.WebFunctions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import bda.hslu.ch.betchain.CallAPI;
+import bda.hslu.ch.betchain.DTO.User;
+import bda.hslu.ch.betchain.WebRequestException;
+
+/**
+ * Created by ssj10 on 23/03/2018.
+ */
+
+public class SettingsFunctions {
+    private static final String SERVER_URL = "http://blockchaincontracts.enterpriselab.ch/index.php";
+
+    public static boolean changeUserSetting(String settingName, String value) throws WebRequestException{
+        try {
+            JSONObject requestParams = new JSONObject();
+            requestParams.put("method", "changeSetting");
+            requestParams.put("settingName", settingName);
+            requestParams.put("value", value);
+
+
+            String response = CallAPI.makePOSTRequestToServer(SERVER_URL, requestParams.toString());
+            JSONObject jsonResponse = new JSONObject(response);
+
+
+            if (jsonResponse.getInt("is_error") == 0) {
+                return true;
+            } else {
+                throw new WebRequestException(jsonResponse.getString("message"));
+            }
+        } catch (JSONException exec) {
+            throw new WebRequestException(exec.getMessage());
+        }
+    }
+}
