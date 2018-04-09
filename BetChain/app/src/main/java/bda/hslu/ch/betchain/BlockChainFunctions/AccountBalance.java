68b54f4b7b236d1650d17b7cd991405a8e47308e
tbhartma <kay.hartmann@stud.hslu.ch>
2018-04-09 14:38:06 +0200

    Account Balance geänadert, holt jetzt infos vom eingeloggten USer

--- BetChain/app/src/main/java/bda/hslu/ch/betchain/BlockChainFunctions/AccountBalance.java
+++ BetChain/app/src/main/java/bda/hslu/ch/betchain/BlockChainFunctions/AccountBalance.java
@@ -22,6 +22,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.concurrent.ExecutionException;
 
+import bda.hslu.ch.betchain.Database.DBSessionSingleton;
 import bda.hslu.ch.betchain.Database.SQLWrapper;
 import bda.hslu.ch.betchain.MainActivity;
 
@@ -33,7 +34,6 @@ import static java.lang.System.err;
 
 public class AccountBalance {
     private String testnet = "https://ropsten.infura.io/";
-    private String walletAddress = "0x6F52912815A67C51BDFdB851ad34F46C7FF4Af8E";
     private BigDecimal eth;
 
     public AccountBalance() {
@@ -42,12 +42,9 @@ public class AccountBalance {
     }
 
     public BigDecimal getAccountBalance(){
-        String[] userInfos;
-        SQLWrapper db = new SQLWrapper();
-        userInfo = db.getLoggedInUserInfo();
         Web3j web3 = Web3jFactory.build(new HttpService(testnet));
         try {
-            EthGetBalance ethGetBalance = web3.ethGetBalance(walletAddress, DefaultBlockParameterName.LATEST).sendAsync().get();
+            EthGetBalance ethGetBalance = web3.ethGetBalance(DBSessionSingleton.getInstance().getDbUtil().getLoggedInUserInfo()[3], DefaultBlockParameterName.LATEST).sendAsync().get();
             BigInteger wei = ethGetBalance.getBalance();
             eth = Convert.fromWei(wei.toString(), Convert.Unit.ETHER);
 
@@ -59,32 +56,6 @@ public class AccountBalance {
         }
 
         return eth;
-
-    }
-
-    public void sendEtherToAccount(){
-
-        Web3j web3 = Web3jFactory.build(new HttpService(testnet));
-
-        Credentials credentials = Credentials.create("b633809e1974f424ffc2888a792c97d9b4ea3070bbfce3e219a7e510df3f7aa1");
-
-        try {
-            TransactionReceipt transactionReceipt = Transfer.sendFunds(
-                    web3, credentials, "0x33Cb1dfB36d0949127EEB70E0792745154e0f01e",
-                    BigDecimal.valueOf(0.1), Convert.Unit.ETHER)
-                    .send();
-
-            }catch (Exception e){
-
-        }
-
-/*
-        transactionObject.setFrom(from);
-        transactionObject.setTo(to);
-        transactionObject.setValue(Convert.toWei("1", Convert.Unit.ETHER).toString());
-        transactionObject.setGas("21000");
-        web3.ethSendTransaction(org.web3j.protocol.core.methods.request.Transaction.)
-        web3.ethSendTransaction(transactionObject.);*/
 
     }
 }
